5e634c9b918da31bc0c0a00300505363b4b3f3ef
KennyMwendwaX <kennymwendwadev@gmail.com>
2024-12-22 17:44:30 +0300

    feat: add Stream to RPC struct

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -6,33 +6,22 @@ import (
 )
 
 type Decoder interface {
-	Decode(io.Reader, *RPC) error
+	Decode(io.Reader, *Message) error
 }
 
 type GOBDecoder struct{}
 
-func (dec GOBDecoder) Decode(r io.Reader, msg *RPC) error {
-	return gob.NewDecoder(r).Decode(msg)
+func (dec GOBDecoder) Decode(r io.Reader, message *Message) error {
+	return gob.NewDecoder(r).Decode(message)
 }
 
 type DefaultDecoder struct{}
 
-func (dec DefaultDecoder) Decode(r io.Reader, msg *RPC) error {
-	peekBuf := make([]byte, 1)
-	if _, err := r.Read(peekBuf); err != nil {
-		return nil
-	}
+func (dec DefaultDecoder) Decode(r io.Reader, msg *Message) error {
+	buf := make([]byte, 1024)
 
-	// In case of a stream we are not decoding what is being sent over the network.
-	// We are just setting Stream true so we can handle that in our logic.
-	stream := peekBuf[0] == IncomingStream
-	if stream {
-		msg.Stream = true
-		return nil
-	}
-
-	buf := make([]byte, 1028)
 	n, err := r.Read(buf)
+
 	if err != nil {
 		return err
 	}
